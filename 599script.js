e9d4883906067926693f3a432d99c6b7ec928234
Nathanielulmerschmit <93093749+Nathanielulmerschmit@users.noreply.github.com>
2021-10-26 21:03:01 -0500

    Initial upload of web files

---
+++ 599script.js
@@ -0,0 +1,14 @@
+function getFormInfo(){
+    const start = document.getElementById("routeStart").value;
+    const end = document.getElementById("routeEnd").value;
+    const route = document.getElementById("txtList").value;
+    let time = document.getElementById("arriveTime").value;
+    
+    var formInfo = [start, end, route, time];
+    processFormInfo(formInfo);
+}
+
+function processFormInfo(formInfo){
+    document.writeln('<p>Your departure stop is ' + formInfo[0] + '. Your destination is ' + formInfo[1] +
+	'. You chose the ' + formInfo[2] + ' route at '+ formInfo[3] +' </p>');  
+}
