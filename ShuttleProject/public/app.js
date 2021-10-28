aa718e4aedc8787b30c0d18f7418df858a41800b
Nathanielulmerschmit <93093749+Nathanielulmerschmit@users.noreply.github.com>
2021-10-27 19:53:16 -0500

    changes

--- ShuttleProject/public/app.js
+++ ShuttleProject/public/app.js
@@ -1,14 +1,6 @@
-function getFormInfo(){
-    const start = document.getElementById("routeStart").value;
-    const end = document.getElementById("routeEnd").value;
-    const route = document.getElementById("txtList").value;
-    let time = document.getElementById("arriveTime").value;
-    
-    var formInfo = [start, end, route, time];
-    processFormInfo(formInfo);
-}
+var express = require('express');
+var app = express();
 
-function processFormInfo(formInfo){
-    document.writeln('<p>Your departure stop is ' + formInfo[0] + '. Your destination is ' + formInfo[1] +
-	'. You chose the ' + formInfo[2] + ' route at '+ formInfo[3] +' </p>');  
-}
+app.listen(3000, () => {
+    console.log('listening on port 3000!');
+})
