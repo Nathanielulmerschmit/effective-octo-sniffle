0f53e596bd5c82631545296f80d5739869fec12b
Nathanielulmerschmit <93093749+Nathanielulmerschmit@users.noreply.github.com>
2021-10-27 20:09:54 -0500

    changes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('ShuttleProject/public'));
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
 app.listen(3000, () => {
     console.log('listening on port 3000!');
 })
