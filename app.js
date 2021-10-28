e018bc9ac2c971ec104c450d9fa8f803524a91c8
Nathanielulmerschmit <93093749+Nathanielulmerschmit@users.noreply.github.com>
2021-10-27 20:03:27 -0500

    changes

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sent('index.html')
-})
-
 app.listen(3000, () => {
     console.log('listening on port 3000!');
 })
-
-
