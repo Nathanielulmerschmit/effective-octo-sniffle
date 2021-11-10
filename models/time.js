f4cff2492c25224df42138e045e81125156c3e28
nathanielulmerschmit <nathanielulmerschmit@gmail.com>
2021-11-10 16:59:00 -0600

    Hopefully the final fix

--- models/time.js
+++ models/time.js
@@ -74,33 +74,30 @@ exports.getDepartTime =  (date, timeArray) => {
     else{
         returnArray.push(`${leaveTime} AM`);
     }
-    returnArray.push(originalHours);
+    returnArray.push(departHour);
     returnArray.push(departMinute);
 
     return returnArray;
 }
 
 exports.getArrivalTime =  (departArray, timeArray) => {
+    let originalHours = departArray[0];
     let hours = departArray[2];
     let nextHour = hours + 1;
     let minutes = departArray[3];
     let leaveTime = "";
     let departHour;
     let departMinute;
-    const originalHours = hours;
     const returnArray = [];
    
-    if(hours > 12){
-        hours = hours - 12;
-        nextHour = nextHour - 12;
-    }
+
 
     hours.toString();
     nextHour.toString();
 
     
     if (minutes < timeArray[0] ){
-        if (timeArray[2] < 10) {
+        if (timeArray[0] < 10) {
             leaveTime = hours + ":0" + timeArray[0];
         }
         else {
@@ -110,7 +107,7 @@ exports.getArrivalTime =  (departArray, timeArray) => {
         departMinute = timeArray[0];
     }
     else if (minutes < timeArray[1]){
-        if (timeArray[2] < 10) {
+        if (timeArray[1] < 10) {
             leaveTime = hours + ":0" + timeArray[1];
         }
         else {
@@ -129,14 +126,21 @@ exports.getArrivalTime =  (departArray, timeArray) => {
         departHour = hours;
         departMinute = timeArray[2];
     }
-    else {
-        if (timeArray[0] >= 10){
-        leaveTime = nextHour + ":" + timeArray[0];}
-
-        else{
-        leaveTime = nextHour + ":0" + timeArray[0];}
+    else if (minutes >= timeArray[2] && ((timeArray[0]+ 60)- minutes >= 5)) {
+        leaveTime = nextHour + ":0" + timeArray[0];
         departHour = nextHour;
         departMinute = timeArray[0];
+    }
+    else {
+        if (timeArray[1] >= 10){
+        leaveTime = nextHour + ":" + timeArray[1];
+    }
+
+        else{
+        leaveTime = nextHour + ":0" + timeArray[1];
+        departHour = nextHour;
+        departMinute = timeArray[1];
+        }
     }
     
     returnArray.push(originalHours);
