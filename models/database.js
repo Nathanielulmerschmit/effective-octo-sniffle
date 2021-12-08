35d2cf29aeee77d7e6610526f752482c40f7121e
nathanielulmerschmit <nathanielulmerschmit@gmail.com>
2021-12-08 16:37:46 -0600

    Revert "trying to fix"
    This reverts commit 7d722290a06f5553b0e60ff0512985270a430bd2.

--- models/database.js
+++ models/database.js
@@ -16,7 +16,6 @@ class Database{
                 if (err) 
                     return reject (err);
                 var times = [];
-                console.log(rows[0]);
                 times.push(rows[0].time1);
                 times.push(rows[0].time2);
                 times.push(rows[0].time3);
