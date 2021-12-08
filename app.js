5895a325ae94adec1426adc96f088ab9b5bcd7f4
nathanielulmerschmit <nathanielulmerschmit@gmail.com>
2021-12-08 17:44:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-//fix
+//
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
